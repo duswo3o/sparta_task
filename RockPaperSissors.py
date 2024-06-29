0dd071f7ec0bd23ad7dcacd26f88604af406e7b6
yeonjae <pyj3740@gmail.com>
2024-06-29 16:11:29 +0900

    플레이어에게 통계 제공

--- RockPaperSissors.py
+++ RockPaperSissors.py
@@ -8,7 +8,7 @@ computer = rsp[random.randint(0,2)] # 컴퓨터의 선택
 
 record = {'승':0, "패": 0, "무승부":0} # 플레이어 통계
 
-
+print(f"사용자: {player}, 컴퓨터: {computer}")
 if player == computer :
     record["무승부"] += 1
     print("무승부!")
@@ -18,3 +18,5 @@ elif (player == '가위' and computer == '보') | (player == '바위' and comput
 else :
     record["패"] += 1
     print('사용자 패배!')
+
+print(f"승 : {record['승']} 패 : {record['패']} 무승부 : {record['무승부']}")
